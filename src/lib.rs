28b2d202b5814eb063e4343b8b729777cecbc9c2
Ed Sweeney <ed@onextent.com>
2019-10-13 07:50:13 -0700

    init

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+extern crate env_logger;
+extern crate log;
+extern crate proc_macro;
+
+// learning todo: ejs remove
+// learning todo: ejs remove
+// learning todo: ejs remove
+
+use quote::quote;
+use syn;
+
+#[proc_macro_derive(HelloMacro)]
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+    // Construct a representation of Rust code as a syntax tree
+    // that we can manipulate
+    let ast = syn::parse(input).unwrap();
+
+    // Build the trait implementation
+    impl_hello_macro(&ast)
+}
+
+fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl HelloMacro for #name {
+            fn hello_macro() {
+                println!("Hello, Macro! My name is {}", stringify!(#name));
+            }
+        }
+    };
+    gen.into()
+}
---
+++ tests/macro_test.rs
@@ -0,0 +1,9 @@
+//! # learning macros
+//!
+//!
+extern crate augorama_derive;
+
+use std::{thread, time};
+
+#[test]
+fn make_route_works() {}
